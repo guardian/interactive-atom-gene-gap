2969f61343a4d689bb875cbd462251731c5b8afa
Garry Blight <garry.blight@guardian.co.uk>
2018-09-07 13:46:14 +0100

    Style amends and hide footer plus randomizer

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,13 @@ var el = document.createElement('script');
 
 el.src = '<%= path %>/app.js';
 document.body.appendChild(el);
+
+
+// UNCOMMENT BELOW FOR HEADER PIC RANDOMIZER
+
+
+// var picNum = Math.floor(Math.random() * 4) + 1;
+
+// var picEl = document.getElementById("gv-header-background");
+// picEl.classList.add("gv-header-image-" + picNum);
+
