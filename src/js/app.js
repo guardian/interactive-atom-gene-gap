71e9f5d97c67650edc82b6bfa85e86219a213121
Garry Blight <garry.blight@guardian.co.uk>
2018-09-11 12:18:19 +0100

    Added shares

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,15 @@
 // javascript goes here
+
+
+import shares from './share'
+
+
+
+[].slice.apply(document.querySelectorAll('.interactive-share')).forEach(shareEl => {
+        var network = shareEl.getAttribute('data-network');
+        shareEl.addEventListener('click', () => shareFn(network));
+    });
+
+
+
+let shareFn = shares('Beyond the Blade', 'https://www.theguardian.com/', '');
